daf618276c40b0868237a90e5b5f8544792948ba
ayelenleclerc <ayelenleclerc@gmail.com>
2022-08-02 09:04:35 -0300

    Fecha terminada

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-const btn = document.querySelector('[data-form-btn]');
+const btn = document.querySelector("[data-form-btn]");
+
+const addTask = (evento) => {
+  const list = document.querySelector("[data-list]");
+  const task = createTask(evento);
+  list.appendChild(task);
+};
 
 const createTask = (evento) => {
   evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
+  const input = document.querySelector("[data-form-input]");
+  const calendar = document.querySelector("[data-form-date]");
   const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
+  const date = calendar.value;
+  const dateFormat = moment(date).format("DD/MM/YYYY");
 
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  //backticks
+  const taskContent = document.createElement("div");
+
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener("click", addTask);
