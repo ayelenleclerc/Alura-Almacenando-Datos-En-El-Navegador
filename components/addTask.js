a5838dabf0dc56fd188c73bef80844e93a213339
ayelenleclerc <ayelenleclerc@gmail.com>
2022-08-03 11:23:43 -0300

    check complete

--- components/addTask.js
+++ components/addTask.js
@@ -1,3 +1,4 @@
+import { uniqueDate } from "../services/date.js";
 import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 import { displayTask } from "./displayTask.js";
@@ -19,30 +20,42 @@ export const addTask = (evento) => {
   input.value = "";
   calendar.value = "";
 
+  const complete = false;
+
   const taskList = localStorage.getItem("tasks")
     ? JSON.parse(localStorage.getItem("tasks"))
     : [];
   const taskObj = {
     value,
     dateFormat,
+    complete,
+    id: uuid.v4(),
   };
   list.innerHTML = "";
-  taskList.push({ value, dateFormat });
+  taskList.push({ value, dateFormat, complete, id: uuid.v4() });
   localStorage.setItem("tasks", JSON.stringify(taskList));
 
   displayTask();
 };
 
-export const createTask = ({ value, dateFormat }) => {
+export const createTask = ({ value, dateFormat, complete, id }) => {
   const task = document.createElement("li");
   task.classList.add("card");
 
   const taskContent = document.createElement("div");
 
+  const check = checkComplete(id);
+  if (complete) {
+    console.log("complete");
+    check.classList.toggle("fas");
+    check.classList.toggle("completeIcon");
+    check.classList.toggle("far");
+  }
+
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(check);
   taskContent.appendChild(titleTask);
   const dateElement = document.createElement("span");
   dateElement.innerHTML = dateFormat;
